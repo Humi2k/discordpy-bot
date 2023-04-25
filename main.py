867686fbba457a276f3120ebfbe211ecf8f2c5f3
Humi2k <82430803+Humi2k@users.noreply.github.com>
2023-04-25 12:07:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,28 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
-
+bot = commands.Bot(command_prefix='>>', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    print("Ready")
 
+#Hello
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def good(ctx):
+    await ctx.send("Hello")
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
-
+# 利用規則
+ID_CHANNEL_README=1099917400704548894
+ID_ROLE_GOOD=1099914039162847232
+@bot.event
+async def on_raw_reaction_add(payload):
+  channel=bot.get_channel(payload.channel_id)
+  if channel.id != ID_CHANNEL_README:
+      return
+  guild = bot.get_guild(payload.guild_id)
+  member = await guild.fetch_member(payload.user_id)
+  role = guild.get_role(ID_ROLE_GOOD)
+  await member.add_roles(role)
 
 bot.run(os.environ["DISCORD_TOKEN"])
